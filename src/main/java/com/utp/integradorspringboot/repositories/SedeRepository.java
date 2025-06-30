401b5758c4f934213f0d2ab330ca07c294534ed8
jriveraa-github <jriveraambrosio@gmail.com>
2025-06-29 23:01:01 -0500

    feat: creación de seeders (data precargada)

--- src/main/java/com/utp/integradorspringboot/repositories/SedeRepository.java
+++ src/main/java/com/utp/integradorspringboot/repositories/SedeRepository.java
@@ -1,12 +1,14 @@
 package com.utp.integradorspringboot.repositories; 
 
-import com.utp.integradorspringboot.models.Sede; 
+import com.utp.integradorspringboot.models.Sede;
 import org.springframework.data.jpa.repository.JpaRepository; 
 import org.springframework.stereotype.Repository; 
 
-import java.util.List; 
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface SedeRepository extends JpaRepository<Sede, Long> { 
+public interface SedeRepository extends JpaRepository<Sede, Long> {
+    Optional<Sede> findByNombreSede(String nombre);
     List<Sede> findByNombreSedeContaining(String nombreSede);
 }
